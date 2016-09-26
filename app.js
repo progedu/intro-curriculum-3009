d81a1bafe81a7893a7cc5ca2caee9f538c555b49
DDRAGON <davide.0829@gmail.com>
2016-09-26 14:29:15 +0900

    writeFile → appendFile

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-fs.writeFile(fileName, 'おはようございます', 'utf8');
-fs.writeFile(fileName, 'こんにちわ', 'utf8');
-fs.writeFile(fileName, 'こんばんわ', 'utf8');
+fs.appendFile(fileName, 'おはようございます', 'utf8');
+fs.appendFile(fileName, 'こんにちわ', 'utf8');
+fs.appendFile(fileName, 'こんばんわ', 'utf8');
