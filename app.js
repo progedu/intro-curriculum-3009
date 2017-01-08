a90d379793e505e248407cf393f85390d8d1ee4b
takuya0206 <tokiwatakuya@gmail.com>
2017-01-08 03:21:48 +0000

    変なスペースを削除

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-fs.	appendFileSync(fileName, 'おはようございます\n', 'utf8');
-fs.	appendFileSync(fileName, 'こんにちは\n', 'utf8');
-fs.	appendFileSync(fileName, 'こんばんは\n', 'utf8');
+fs.appendFileSync(fileName, 'おはようございます\n', 'utf8');
+fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');
+fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
