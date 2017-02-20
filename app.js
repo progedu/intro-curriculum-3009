ef3aaea6e08f95edb3a250a23b33b7cdc3cecd7c
KaiKogure <kai.kogure@live.jp>
2017-02-20 14:27:51 +0000

    for 文とsyncの追加

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-fs.appendFile(fileName, 'おはようございます\n', 'utf8');
-fs.appendFile(fileName, 'こんにちは\n', 'utf8');
-fs.appendFile(fileName, 'こんばんは\n', 'utf8');
+for (let count = 0; count < 500; count++) {
+fs.appendFileSync(fileName, 'おはようございます\n', 'utf8');
+fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');
+fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
+
+}
