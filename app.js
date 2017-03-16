4724ac8f6da9c65476d351ac69b3d1a9e70d0536
DDRAGON <davide.0829@gmail.com>
2017-03-16 16:53:59 +0900

    30回くらいループするよう修正

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-fs.appendFile(fileName, 'おはようございます\n', 'utf8');
-fs.appendFile(fileName, 'こんにちは\n', 'utf8');
-fs.appendFile(fileName, 'こんばんは\n', 'utf8');
+for (let count = 0; count < 30; count++) {
+    fs.appendFile(fileName, 'おはようございます\n', 'utf8');
+    fs.appendFile(fileName, 'こんにちは\n', 'utf8');
+    fs.appendFile(fileName, 'こんばんは\n', 'utf8');
+}
