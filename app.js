02cbe1e4cdc89c1a79d193f8f8afc6537a91ed20
Mono-Alex <nagao.yoshiki.exe@gmail.com>
2017-09-18 07:52:58 +0000

    連続追記の機能を削除の実装

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
+const fileBoolean = fs.existsSync(fileName);
+
+if (fileBoolean == true) {
+    fs.unlink(fileName);//fileが初めからあるとtrueで削除
+    console.log('file delete');
+} else {
+    console.log('file not delete');
+}
+
+console.log('file created');
+
 for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8');
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8');
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8');
+    fs.appendFileSync(fileName, 'おはようございます\n', 'utf8');
+    fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');
+    fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
 }
