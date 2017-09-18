ba3a3ffb2d4bc2652b0bb7659e032d3e47724493
meganeneko <ge2hoka@yahoo.co.jp>
2017-09-18 11:19:35 +0000

    非同期I/Oを同期I/Oに変更

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const fs = require('fs');
 const fileName = './test.txt';
 for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8');
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8');
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8');
-}
+    fs.appendFileSync(fileName, '(*^-^*)｜お・は・よ・う・！｜(*^-^*)\n', 'utf8');
+    fs.appendFileSync(fileName, '(#^.^#)｜こ・ん・に・ち・は｜(#^.^#)\n', 'utf8');
+    fs.appendFileSync(fileName, '(*^_^*)｜こ・ん・ば・ん・は｜(*^_^*)\n', 'utf8');
+    fs.appendFileSync(fileName, '(*´ω`*)｜お・や・す・み・ｚ｜(*´ω`*)\n', 'utf8');
+}　
