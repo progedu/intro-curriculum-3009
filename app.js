f3eed075074a4ad5852e0b4c892e8f612a8837ae
hase1108 <hase1108shoiti@yahoo.co.jp>
2018-05-15 13:04:57 +0000

    練習問題

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const fileName = './test.txt';
 for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8');
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8');
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8');
+    fs.appendfileSync(fileName, 'おはようございます\n', 'utf8');
+    fs.appendfileSync(fileName, 'こんにちは\n', 'utf8');
+    fs.appendfileSync(fileName, 'こんばんは\n', 'utf8');
 }
