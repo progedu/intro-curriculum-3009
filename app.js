7c3c9abc3d06700e0bd8d01b0642c7761ed54bee
re-ryonchil <41006283+re-ryonchil@users.noreply.github.com>
2018-07-13 18:51:04 +0900

    非同期I/Oを同期I/Oに変更

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8');
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8');
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8');
+for (let count = 0; count < 500; count++) {
+	fs.appendFileSync(fileName, 'あ', 'utf8');
+	fs.appendFileSync(fileName, 'い', 'utf8');
+	fs.appendFileSync(fileName, 'う', 'utf8');
+	fs.appendFileSync(fileName, 'え', 'utf8');
+	fs.appendFileSync(fileName, 'お', 'utf8');
+	fs.appendFileSync(fileName, '\n', 'utf8');
 }
