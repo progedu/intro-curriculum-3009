9bf729ee10fe7944c155b53bdb331dff526c1e62
nlnlnul <walking_5km@yahoo.co.jp>
2018-11-12 10:04:55 +0000

    書き込み処理を同期 I/O に変更

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
-const fs = require('fs');
-const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8');
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8');
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8');
+const fs = require('fs'); //ファイルシステムを取り扱うモジュールを読み込んでいるコード
+const fileName = './test.txt'; //test.txt というファイルに書き込む
+for (let count = 0; count < 30; count++) { //30回書き込む。\nは改行
+    fs.appendFileSync(fileName, 'おはようございます\n', 'utf8');
+    fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');
+    fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
 }
