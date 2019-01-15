1ea5e44849d33a81b2441fd4cc2fdd2a4578f8a8
take <thibodeau9@gmail.com>
2019-01-15 15:15:15 +0000

    change appendFile to appendFileSync

---
+++ .history/app_20190116000443.js
@@ -0,0 +1,8 @@
+'use strict';
+const fs = require('fs');
+const fileName = './test.txt';
+for (let count = 0; count < 30; count++) {
+    fs.appendFile(fileName, 'おはようございます\n', 'utf8');
+    fs.appendFile(fileName, 'こんにちは\n', 'utf8');
+    fs.appendFile(fileName, 'こんばんは\n', 'utf8');
+}
