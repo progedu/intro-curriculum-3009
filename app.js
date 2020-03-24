fd7d269226db606acca3ec3977706ea798367ab5
hir810 <hir81054@gmail.com>
2020-03-25 02:01:12 +0900

    同期I/Oファイル書き込みに変更

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 const fileName = './test.txt';
 for (let count = 0; count < 30; count++) {
     fs.appendFileSync(fileName, 'おはようございます\n', 'utf8');
-    fs.appendFileSync(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFileSync(fileName, 'こんばんは\n', 'utf8', () => {});
+    fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');
+    fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
 }
