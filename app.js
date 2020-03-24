bbac64732b9e80487d97f3bf4dc95e4776f70a5a
chip-cocoa2020 <arkbuchessener1209@outlook.jp>
2020-03-25 03:24:53 +0900

    console上で文字の読込にかかった時間の表記を少し改変

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-console.time('ファイルの読込処理');
+console.time('文字の読込処理時間');
 for (let count = 0; count < 50; count++) {
     fs.appendFileSync(fileName, 'おはようございます\n', 'utf8');
     fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');
     fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
     console.log(` count = ${count}`);
 }
-console.timeEnd('ファイルの読込処理');
+console.assert(fileName, 'ファイルの読込に失敗しました。');
+console.timeLog('文字の読込処理時間');
