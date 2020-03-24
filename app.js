6ad021a146d35940716d5671a977442ec2452ab2
chip-cocoa2020 <arkbuchessener1209@outlook.jp>
2020-03-25 02:53:38 +0900

    ファイル読込処理にかかった時間をミリ秒でconsole上に表示する機能を実装

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
+console.time('ファイルの読込処理');
+for (let count = 0; count < 50; count++) {
     fs.appendFileSync(fileName, 'おはようございます\n', 'utf8');
     fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');
     fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
+    console.log(` count = ${count}`);
 }
+console.timeEnd('ファイルの読込処理');
