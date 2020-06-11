a53db66d008a4dcd255fb8d6d4f89b6548a9b804
yuto555dx <yuzentaito@gmail.com>
2020-06-11 11:25:15 +0900

    同期間数を使うように変更

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const fs = require('fs');
 const fileName = './test.txt';
 for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+    fs.appendFileSync(fileName, 'おはようございます、胃もたれしやすいので朝ご飯は抜くかな。\n', 'utf8');
+    fs.appendFileSync(fileName, 'こんにちは、朝を抜いたのでお腹がすいた！かつ丼食べたい！\n', 'utf8');
+    fs.appendFileSync(fileName, 'こんばんは、風呂入って寝る！歯も磨かなきゃ！\n', 'utf8');
+    console.log(`count = ${count}`);
 }
