bf57c5d05bb7798f9d208b512a4bddbaf12250b6
吉田稲作 <yoshidanoinasaku@gmail.com>
2020-06-15 23:37:27 +0900

    非同期IOを同期IOに変更する

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
-const fs = require('fs');
-const fileName = './test.txt';
+const fs = require('fs');    // ファイルを読み込むモジュールをインポート
+const fileName = './test.txt';    // ファイル text.txt に書き込み（./ で同じディレクトリ配下に作成）
 for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+    fs.appendFileSync(fileName, 'おはようございます\n', 'utf8');    // 非同期IOの際は引数にコールバック関数が必須
+    fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');    // Syncを使う際はコールバック関数は不要
+    fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
 }
