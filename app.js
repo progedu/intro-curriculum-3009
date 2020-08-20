6cd8bd42f0575f87c3602125af6e43ff017a9886
kei0808 <keicake0808@gmail.com>
2020-08-20 21:05:27 +0900

    修正

--- app.js
+++ app.js
@@ -5,4 +5,13 @@ for (let count = 0; count < 30; count++) {
     fs.appendFileSync(fileName, 'おはようございます\n', 'utf8');
     fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');
     fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
+    fs.appendFileSync(fileName,'さようなら\n','utf8');
+    fs.appendFileSync(fileName,'また明日\n','utf8');
+    fs.appendFileSync(fileName,'あ\n','utf8');
+    fs.appendFileSync(fileName,'   い\n','utf8');
+    fs.appendFileSync(fileName,'      う\n','utf8');
+    fs.appendFileSync(fileName,'         え\n','utf8');
+    fs.appendFileSync(fileName,'            お\n','utf8');
+ 
+
 }
