2eb7e9a7d48652994f1d636c4b2a902090879d12
Hirohito Komura <hiro.komura@gmail.com>
2020-08-25 19:25:37 +0900

    async/awaitを用いて非同期処理を実装

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+
+function appendFilePromise(fileName, str1, str2) {
+    return new Promise((resolve, reject) => {
+        fs.appendFile(fileName, str1, str2, () => resolve());
+    });
 }
+
+async function main() {
+    for (let count = 0; count < 30; count++) {
+        await appendFilePromise(fileName, 'おはようございます\n', 'utf8');
+        await appendFilePromise(fileName, 'こんにちは\n', 'utf8');
+        await appendFilePromise(fileName, 'こんばんは\n', 'utf8');
+    }
+}
+
+main();
