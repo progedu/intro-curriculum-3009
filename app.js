22a26def575765a7e305d56275047f5b690662ed
heavyfox <steamtrader@gmai.com>
2020-10-06 20:10:35 +0900

    書き込み処理を非同期I/Oにasync/awaitを組み合わせました

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-    fs.appendFileSync(fileName, 'おはようございます\n', 'utf8', () => {});
-    fs.appendFileSync(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFileSync(fileName, 'こんばんは\n', 'utf8', () => {});
-    console.log(`count =${count}`);
-}
+function appendFilePromise(fileName, str) {
+    return new Promise((resolve) => {
+      fs.appendFile(fileName, str, 'utf8', () => resolve());
+    });
+  }
+  async function main() {
+    for (let count = 0; count < 30; count++) {
+      await appendFilePromise(fileName, 'おはようございます\n', 'utf8',);
+      await appendFilePromise(fileName, 'こんにちは\n', 'utf8',);
+      await appendFilePromise(fileName, 'こんばんは\n', 'utf8',);
+      console.log(`count =${count}`);
+    }
+  }
+  
+  main();
