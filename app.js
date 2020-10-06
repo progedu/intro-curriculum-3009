55f93682d3020330db362dae2aff011143a6afe3
blue-one1975 <hiroshitakeda428@yahoo.co.jp>
2020-10-06 20:20:17 +0900

    async/awaitでの同期I/Oへの変更

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+// for (let count = 0; count < 30; count++) {
+//     fs.appendFileSync(fileName, 'おはようございます\n', 'utf8');
+//     fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');
+//     fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
+// }
+
+function appendFilepromise(fileName, str) {
+  return new Promise(resolve => {
+    fs.appendFile(fileName, str, 'utf8', () => resolve());
+  });
 }
+// let promise = new Promise(resolve => { resolve(); });
+// for(let i = 0; i < 30; i++) {
+//   promise = promise.then(() => {
+//     return appendFilepromise(fileName, 'おはようございます\n');
+//   }).then(() => {
+//     return appendFilepromise(fileName, 'こんにちは\n');
+//   }).then(() => {
+//     return appendFilepromise(fileName, 'おばんです\n');
+//   });
+// }
+
+async function main() {
+  for (let count = 30; 0 < count; count--) {
+    await appendFilepromise(fileName, `${count}\n`);
+    await appendFilepromise(fileName, 'おはようございます\n');
+    await appendFilepromise(fileName, 'こんにちは\n');
+    await appendFilepromise(fileName, 'おばんです\n');
+  };
+}
+main();
