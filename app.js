d4a707882ba294b915ff57008af861216389b273
natumenonomi <nonegemu3e@gmail.com>
2020-10-06 20:22:28 +0900

    非同期処理I/Oを同期I/Oに変更（Promiseとasync/await組み合わせ）

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
+
+function appendFilePromise(fileName, str) {
+  return new Promise((resolve) => {
+    fs.appendFile(fileName, str, 'utf8', () => resolve());
+  });
+}
+async function main(){
 for (let count = 0; count < 30; count++) {
-  fs.appendFileSync(fileName, 'おはようございます\n', 'utf8', () => {});
-  fs.appendFileSync(fileName, 'こんにちは\n', 'utf8', () => {});
-  fs.appendFileSync(fileName, 'こんばんは\n', 'utf8', () => {});
+  await appendFilePromise(fileName, 'おはようございます\n', 'utf8', () => {});
+  await appendFilePromise(fileName, 'こんにちは\n', 'utf8', () => {});
+  await appendFilePromise(fileName, 'こんばんは\n', 'utf8', () => {});
+ }
 }
