68f205843a217faecb6acbdf5f348d8f432c370c
ttmone <tomone.ameno@gmail.com>
2020-10-07 16:14:53 +0900

    同期処理の実装

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 'use strict';
 const fs = require('fs');
+const { resolve } = require('path');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-    fs.appendFileSync(fileName, 'おはようございます\n', 'utf8', () => {});
-    fs.appendFileSync(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFileSync(fileName, 'こんばんは\n', 'utf8', () => {});
+
+function appendFilePromise(fileName, str) {
+    return new Promise((resolve) => {
+        fs.appendFile(fileName, str, 'utf-8', () => {resolve()});
+    });
 }
+
+async function main() {
+    for (let count = 0; count < 30; count++) {
+        await appendFilePromise(fileName, 'おはようございます\n');
+        await appendFilePromise(fileName, 'こんにちは\n');
+        await appendFilePromise(fileName, 'こんばんは\n');
+    }    
+}
+
+main();
