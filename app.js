f9a64563652badf3ad4098be2fc2ea5c1eb3f5f1
shinfujita <shin4522@gmail.com>
2020-10-14 16:07:25 +0900

    3章10節の課題

--- app.js
+++ app.js
@@ -6,3 +6,37 @@ for (let count = 0; count < 30; count++) {
     fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');
     fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
 }
+
+
+// promise
+// function appendFilePromise(fileName, str) {
+//     return new Promise((resolve) => {
+//         fs.appendFile(fileName, str, 'utf8', () => resolve());
+//     });
+// }
+
+// for (let count = 0; count < 30; count++) {
+//     appendFilePromise(fileName, 'おはようございます\n')
+//     .then(() => {
+//         return appendFilePromise(fileName, 'こんにちは\n');
+//     })
+//     .then(() => {
+//         return appendFilePromise(fileName, 'こんばんは\n');
+//     })
+// }
+
+
+// async/await
+// function appendFilePromise(fileName, str) {
+//     return new Promise((resolve) => {
+//         fs.appendFile(fileName, str, 'utf8', () => resolve());
+//     });
+// }
+// async function main() {
+//     for (let count = 0; count < 30; count++) {
+//         await appendFilePromise(fileName, 'おはようございます\n');
+//         await appendFilePromise(fileName, 'こんにちは\n');
+//         await appendFilePromise(fileName, 'こんばんは\n');
+//     }
+// }
+// main();
