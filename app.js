f1c5bebd304df793ad5a64af07c6eaea9f145223
gomi <assassincreed0461-gta005@outlook.jp>
2020-10-20 14:20:11 +0900

    Promise を使った方法を追加しました。

--- app.js
+++ app.js
@@ -1,17 +1,46 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
+
+// async\await を使った同期処理の方法
+    
+    /*function appendFilePromise(fileName, str) {
+        return new Promise((resolve) => {
+            fs.appendFile(fileName, str, 'utf8', () => resolve());
+        })
+    }
+
+    async function main() {
+        for (let count = 0; count < 30; count++) {
+            await appendFilePromise(fileName, 'おはようございます\n');
+            await appendFilePromise(fileName, 'こんにちは\n');
+            await appendFilePromise(fileName, 'こんばんは\n');
+        }
+    }
+
+    main();
+*/
+
+// Promise を使った同期処理の方法
 function appendFilePromise(fileName, str) {
     return new Promise((resolve) => {
         fs.appendFile(fileName, str, 'utf8', () => resolve());
-    })
+    });
 }
 
-async function main() {
-    for (let count = 0; count < 30; count++) {
-        await appendFilePromise(fileName, 'おはようございます\n');
-        await appendFilePromise(fileName, 'こんにちは\n');
-        await appendFilePromise(fileName, 'こんばんは\n');
+function main() {
+    let promiseChain = Promise.resolve();
+    for (let count = 0; count < 500; count++) {
+        promiseChain = promiseChain
+            .then(() => {
+                return appendFilePromise(fileName, 'おはようございます\n');
+            })
+            .then(() => {
+                return appendFilePromise(fileName, 'こんにちは\n');
+            })
+            .then(() => {
+                return appendFilePromise(fileName, 'こんばんは\n');
+            })
     }
 }
 
