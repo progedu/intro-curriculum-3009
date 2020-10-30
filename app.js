c202df7ee40092204d35203abcc8db3fb35553da
Yuya Satake <sata.yu.7878@gmail.com>
2020-10-31 05:02:18 +0900

    async/await処理の実装

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+
+function makeFile(fileName, str) {
+    return new Promise((resolve) => {
+        fs.appendFile(fileName, str, 'utf-8', () => resolve())
+    });
+ }
+
+async function main() {
+    for (let count = 0; count < 30; count++){
+        await makeFile(fileName, 'おはようございます\n');
+        await makeFile(fileName, 'こんにちは\n');
+        await makeFile(fileName, 'こんばんは\n');
+    }
 }
+ 
+main();
