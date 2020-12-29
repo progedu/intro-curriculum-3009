510178ffab6499e8f5c0ff72cd33fbce806fe9fe
Yu Kumagai <coffeekumasan@gmail.com>
2020-12-29 20:03:00 +0900

    非同期 I/O のものから同期 I/O へ変更

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+
+function appendFilePromise(fileName, str) {
+    return new Promise((resolve) => {
+      fs.appendFile(fileName, str, 'utf8', () => resolve());
+    });
+  }
+async function main() {
+  for (let count = 0; count < 30; count++) {
+    await appendFilePromise(fileName, 'おはようございます\n', 'utf8', () => {});
+    await appendFilePromise(fileName, 'こんにちは\n', 'utf8', () => {});
+    await appendFilePromise(fileName, 'こんばんは\n', 'utf8', () => {});
+  }
 }
+
+main();
