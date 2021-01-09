20124b4062a658fb80b38955ca6ec9f3e25456ec
sho01072020 <takagi.s3637@gmail.com>
2021-01-09 17:26:12 +0900

    Promise Chain と await/async の実装

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 'use strict';
 const fs = require('fs');
+const { resolve } = require('path');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+
+//
+// Promise Chain
+//
+function  appendFilePromiseChain(fileName, str) {
+    return new Promise((resolve) => {
+        fs.appendFile(fileName, str, 'utf8', () => resolve());
+    })
 }
+
+function main1() {
+    for(let count = 0; count < 30; count++){
+        appendFilePromiseChain(fileName, 'Good Morning\n')
+            .then(() => {
+                return appendFilePromiseChain(fileName, 'Hello\n');
+            })
+            .then(() => {
+                return appendFilePromiseChain(fileName, 'Good Evening\n');
+            });
+    }    
+}
+
+//
+// async / await ver.
+//
+function appendFilePromise(filename, str) {
+    return new Promise((resolve) => {
+        fs.appendFile(fileName, str, 'utf8', () => resolve());
+    });
+}
+
+async function main2() {
+    for (let count = 0; count < 30; count++) {
+        await appendFilePromise(fileName, 'おはようございます\n');
+        await appendFilePromise(fileName, 'こんにちは\n');
+        await appendFilePromise(fileName, 'こんばんは\n');
+    }    
+}
+
+main1();
