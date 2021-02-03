7baa2a2868003b57461630a08f409e940fc78c88
mizutama6 <pajama.blog0+github20@gmail.com>
2021-02-03 21:18:54 +0900

    ファイル書き込み処理を同期I/Oに変更

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
+
+// appendFileSync 利用
 for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+    fs.appendFileSync(fileName, 'おはようございます\n', 'utf8', () => {});
+    fs.appendFileSync(fileName, 'こんにちは\n', 'utf8', () => {});
+    fs.appendFileSync(fileName, 'こんばんは\n', 'utf8', () => {});
 }
+
+// async/await 利用
+const outputToFile = (text) => {
+    fs.appendFile(fileName, text, 'utf8', () => {});
+}
+async function main() {
+    for (let count = 0; count < 30; count++) {
+        await outputToFile('おはようございます\n');
+        await outputToFile('こんにちは\n');
+        await outputToFile('こんばんは\n');
+    }
+}
+main();
