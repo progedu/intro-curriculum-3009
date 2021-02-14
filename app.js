c010ba7da25a72db68c020e7a24a861d1eabf0c8
senarice <tarahukugohan@docomo.ne.jp>
2021-02-15 00:35:55 +0900

    async/awaitを用いた実装 真

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
+function appendFilePromise(fileName,str){
+    return new Promise((resolve) => {
+        fs.appendFile(fileName,str,'utf8', () => resolve());
+    })
+}
 
 async function main(){
     for (let count = 0; count < 30; count++) {
-       await fs.appendFileSync(fileName, 'おはようございます\n', 'utf8', () => {});
-       await fs.appendFileSync(fileName, 'こんにちは\n', 'utf8', () => {});
-       await fs.appendFileSync(fileName, 'こんばんは\n', 'utf8', () => {});
+       await fs.appendFilePromise(fileName, 'おはようございます\n', 'utf8', () => {});
+       await fs.appendFilePromise(fileName, 'こんにちは\n', 'utf8', () => {});
+       await fs.appendFilePromise(fileName, 'こんばんは\n', 'utf8', () => {});
     }
 }
 
