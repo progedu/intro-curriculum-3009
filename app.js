11ddba9a6a9b1e6f8d4c5321cd7c0bafb557eac6
Ryo <moribon50@gmail.com>
2021-03-08 11:15:17 +0900

    非同期 I/O のものから同期 I/O のものに変更

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+function appendFilePromise(fileName, str) {
+    return new Promise((resolve) => {
+        fs.appendFile(fileName, str, 'utf8', () => resolve());
+    });
 }
+
+function main() {
+    let promiseChain = Promise.resolve(); 
+    for (let count = 0; count < 30; count++) {
+        promiseChain = promiseChain
+            .then(() => {
+                return appendFilePromise(fileName, 'おはようございます\n');
+            })
+            .then(() => {
+                return appendFilePromise(fileName, 'こんにちは\n');
+            })
+            .then(() => {
+                return appendFilePromise(fileName, 'こんばんは\n');
+            });
+    }
+}
+
+main();
+
+
