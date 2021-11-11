4b2839092d58cedf5a3410b1f8829d44c04b81e6
Mitsuo Sato <mitti310@gmail.com>
2021-11-11 13:30:20 +0900

    練習問題実装

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
-for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8', () => {});
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8', () => {});
+
+function appendFilePromise(fileName, str) {
+  return new Promise((resolve) => {
+    fs.appendFile(fileName, str, 'utf8', () => resolve());
+  });
 }
+async function main() {
+  for (let count = 0; count < 500; count++) {
+    await appendFilePromise(fileName, 'あ');
+    await appendFilePromise(fileName, 'い');
+    await appendFilePromise(fileName, 'う');
+    await appendFilePromise(fileName, 'え');
+    await appendFilePromise(fileName, 'お');
+    await appendFilePromise(fileName, '\n');
+  }
+}
+  
+main();
