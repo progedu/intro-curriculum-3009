e6f7c0f5a4be67512bcceddd0cf2a0263b867661
kkomeng <komix68@gmail.com>
2023-10-05 20:58:45 +0900

    同期i/oに変更

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 'use strict';
 const fs = require('fs');
 const fileName = './test.txt';
+const cb = (err) => {
+    if (err) {
+        console.log(err);
+    };
+};
 for (let count = 0; count < 30; count++) {
-    fs.appendFile(fileName, 'おはようございます\n', 'utf8');
-    fs.appendFile(fileName, 'こんにちは\n', 'utf8');
-    fs.appendFile(fileName, 'こんばんは\n', 'utf8');
+    fs.appendFileSync(fileName, 'おはようございます\n', 'utf8');
+    fs.appendFileSync(fileName, 'こんにちは\n', 'utf8');
+    fs.appendFileSync(fileName, 'こんばんは\n', 'utf8');
 }
